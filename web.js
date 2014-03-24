89a47bf65dbee71d2f069fa22741801987c15385
GarySwift <garyswiftmail@gmail.com>
2014-03-24 21:53:41 +0400

    Added node with express and logfmt

--- web.js
+++ web.js
@@ -1,1 +1,17 @@
-console.log("Node Tester");
+// web.js
+// https://github.com/GarySwift/node_tester_01.git
+
+var express = require("express");
+var logfmt = require("logfmt");
+var app = express();
+
+app.use(logfmt.requestLogger());
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
